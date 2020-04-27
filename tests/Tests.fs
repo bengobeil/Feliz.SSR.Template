9ba024801f42d67c1a6992f342107a4d890e0d62
Benjamin Gobeil <ben_gob@hotmail.com>
2020-04-27 13:58:34 -0400

    Make test compile

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,21 +1,22 @@
-module Tests
+namespace Template
 
-open Fable.Mocha
-open App
+module Tests =
+    open Fable.Mocha
+    open Shared
 
-let appTests = testList "App tests" [
-    testCase "update function works" <| fun _ ->
-        // Simplified update that ignore commands/effects
-        let update state msg = fst (App.update msg state)
-        let initialState = { Count = 0 }
-        let incomingMsgs =  [ Increment; Increment; Decrement ]
-        let updatedState = List.fold update initialState incomingMsgs
-        Expect.equal 1 updatedState.Count "Expected updated state to be 1"
-]
+    let appTests = testList "App tests" [
+        testCase "update function works" <| fun _ ->
+            // Simplified update that ignore commands/effects
+            let update state msg = fst (App.update msg state)
+            let initialState = { Count = 0 }
+            let incomingMsgs =  [ Increment; Increment; Decrement ]
+            let updatedState = List.fold update initialState incomingMsgs
+            Expect.equal 1 updatedState.Count "Expected updated state to be 1"
+    ]
 
-let allTests = testList "All" [
-    appTests
-]
+    let allTests = testList "All" [
+        appTests
+    ]
 
-[<EntryPoint>]
-let main (args: string[]) = Mocha.runTests allTests
+    [<EntryPoint>]
+    let main (args: string[]) = Mocha.runTests allTests
