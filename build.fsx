02bcb0546fdf464db426f39bf54ce0022348d867
Benjamin Gobeil <ben_gob@hotmail.com>
2020-04-27 13:20:21 -0400

    Use fake on top of npm

--- build.fsx
+++ build.fsx
@@ -1,3 +1,7 @@
+#if !FAKE
+#r "netstandard"
+#endif
+
 #r "paket:
 nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
@@ -12,21 +16,90 @@ open Fake.Core.TargetOperators
 
 Target.initEnvironment ()
 
+let clientPath = Path.getFullName "./src/App"
+
+let platformTool tool winTool =
+    let tool = if Environment.isUnix then tool else winTool
+    match ProcessUtils.tryFindFileOnPath tool with
+    | Some t -> t
+    | _ ->
+        let errorMsg =
+            tool + " was not found in path. " +
+            "Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+        failwith errorMsg
+        
+let nodeTool = platformTool "node" "node.exe"
+let npmTool = platformTool "npm" "npm.cmd"
+
+let runTool cmd args workingDir =
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    Command.RawCommand (cmd, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+let runDotNet cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+   
+let openBrowser url =
+    //https://github.com/dotnet/corefx/issues/10361
+    Command.ShellCommand url
+    |> CreateProcess.fromCommand
+    |> CreateProcess.ensureExitCodeWithMessage "opening browser failed"
+    |> Proc.run
+    |> ignore
+
 Target.create "Clean" (fun _ ->
     !! "src/**/bin"
     ++ "src/**/obj"
     |> Shell.cleanDirs 
 )
 
-Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+Target.create "InstallClient" (fun _ ->
+    printfn "Node version:"
+    runTool nodeTool "--version" __SOURCE_DIRECTORY__
+    printfn "Npm version:"
+    runTool npmTool "--version"  __SOURCE_DIRECTORY__
+    runTool npmTool "install" __SOURCE_DIRECTORY__
+    runDotNet "restore" clientPath
 )
 
-Target.create "All" ignore
+
+Target.create "Run" (fun _ ->
+    let client = async {
+        runTool npmTool "start" __SOURCE_DIRECTORY__
+    }
+    let browser = async {
+        do! Async.Sleep 5000
+        openBrowser "http://localhost:8080"
+    }
+    let vsCodeSession = Environment.hasEnvironVar "vsCodeSession"
+
+    let tasks =
+        [ yield client
+          if not vsCodeSession then yield browser ]
+        
+    tasks
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+)
+
+Target.create "Build" (fun _ ->
+    runTool npmTool "run build" __SOURCE_DIRECTORY__
+)
 
 "Clean"
-  ==> "Build"
-  ==> "All"
+    ==> "InstallClient"
+    ==> "Build"
 
-Target.runOrDefault "All"
+"Clean"
+    ==> "InstallClient"
+    ==> "Run"
+
+Target.runOrDefaultWithArguments "Build"
